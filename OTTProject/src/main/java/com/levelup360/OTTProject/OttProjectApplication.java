cb21b4b71a41e5ce59a1942c2c765ab9bf998202
Level_Up_ 360 <dronakapil@gmail.com>
2025-04-16 14:46:29 +0530

    Added OTTProject module

---
+++ OTTProject/src/main/java/com/levelup360/OTTProject/OttProjectApplication.java
@@ -0,0 +1,13 @@
+package com.levelup360.OTTProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttProjectApplication.class, args);
+	}
+
+}
---
+++ OTTProject/src/test/java/com/levelup360/OTTProject/OttProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup360.OTTProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
